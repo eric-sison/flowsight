862ed93758a7061ea75d36793584fd5bf64ff054
eric-sison <ericsison.dev@gmail.com>
2025-08-27 09:09:21 +0800

    enabled react compiler

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"], // include domain names and/or IP addresses for local dev server
   experimental: {
     optimizePackageImports: ["lucide-react", "date-fns"],
+    reactCompiler: true,
   },
 };
 
